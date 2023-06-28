5c03d932979915c7d350809dcf5cfad8616712f0
santigarciab <santiagogarcia@tutanota.com>
2023-06-28 19:38:34 -0300

    javascript

---
+++ programacion.js
@@ -0,0 +1,12 @@
+function Agrandar(Rostro) {
+    let foto = document.getElementById(Rostro);
+    foto.style.width = "20em";
+    foto.style.float = "inline-end"
+}
+
+function Alejar(Rostro) {
+    let foto = document.getElementById(Rostro);
+    foto.style.width = "10em";
+    foto.style.float = "left";
+}
+
